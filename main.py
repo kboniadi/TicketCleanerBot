c712dd2a54775b701c121c63de1a9611d4edbcad
Kord Boniadi <kordbon1@gmail.com>
2023-04-18 16:48:47 -0700

    discord ticket cleaner bot

---
+++ main.py
@@ -0,0 +1,4 @@
+from ticket_cleaner_bot import run_ticket_tool_bot
+
+if __name__ == "__main__":
+    run_ticket_tool_bot()
---
+++ ticket_cleaner_bot.py
@@ -0,0 +1,51 @@
+import asyncio
+import os
+from datetime import datetime, timedelta, timezone
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN") or ""
+
+
+async def clear_inactive_tickets(client: 'discord.Client'):
+    for channel in client.get_all_channels():
+        if channel.name.startswith("ticket"):
+            if not isinstance(channel, discord.TextChannel):
+                continue
+            # get latest message
+            async for message in channel.history(limit=1):
+                if message.author.bot:
+                    continue
+
+                time_diff = datetime.now().astimezone(tz=timezone.utc) - message.created_at
+                # Check if message is older than 48 hours
+                if time_diff > timedelta(hours=48):
+                    await channel.delete(reason="stale ticket")
+                    break
+
+
+def run_ticket_tool_bot():
+    intent = discord.Intents.default()
+    intent.message_content = True
+    client = discord.Client(intents=intent)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+        while True:
+            # Wait for 48 hours
+            await asyncio.sleep(60 * 60 * 24 * 2)  
+            # Clear inactive tickets every 48 hours
+            await clear_inactive_tickets(client=client)  
+    
+    @client.event
+    async def on_message(message: 'discord.Message'):
+        if message.author == client.user:
+            return
+
+        if message.content == '?clear_all':
+            await clear_inactive_tickets(client=client)
+
+    client.run(TOKEN)
